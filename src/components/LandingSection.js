0d746a5f95863b19ee7ffc05da580dd2af4250ab
achrefabdennebi <achref.abdennebi@gmail.com>
2024-02-29 22:30:35 +0100

    feat: add section 1

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Avatar, Heading, VStack } from "@chakra-ui/react";
+import {Avatar, Box, Heading, VStack} from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 
 const greeting = "Hello, I am Pete!";
@@ -8,15 +8,26 @@ const bio2 = "specialised in React";
 
 // Implement the UI for the LandingSection component according to the instructions.
 // Use a combination of Avatar, Heading and VStack components.
-const LandingSection = () => (
-  <FullScreenSection
+const LandingSection = () => (<FullScreenSection
     justifyContent="center"
     alignItems="center"
     isDarkBackground
     backgroundColor="#2A4365"
-  >
-
-  </FullScreenSection>
-);
+>
+    <Avatar src="https://i.pravatar.cc/150?img=7"/>
+    <VStack spacing={14}>
+        <Heading as="h3" size="sm">
+            {greeting}
+        </Heading>
+        <Box>
+            <Heading as="h1" size="2xl">
+                {bio1}
+            </Heading>
+            <Heading as="h1" size="2xl">
+                {bio2}
+            </Heading>
+        </Box>
+    </VStack>
+</FullScreenSection>);
 
 export default LandingSection;
